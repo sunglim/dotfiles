9e23671e2c18aa281bafc045e98b95765d675dbe
Lim Sungguk <limasdf@gmail.com>
2013-08-12 20:51:46 +0900

    Update for bundle install

--- setup.sh
+++ setup.sh
@@ -8,3 +8,9 @@ fi
 
 echo 'Copy .screenrc to ROOT'
 cp $CUR_DIR/.screenrc $HOME/.screenrc
+
+echo 'clone vundle.. '
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+echo 'Install plugins declared at Bundle'
+vim +BundleInstall +qall
