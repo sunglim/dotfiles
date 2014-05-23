f5663d1f3ec40074fb9673b741ebc5f00c241b2a
Sungguk Lim <limasdf@gmail.com>
2014-05-22 17:16:37 -0700

    rearrange

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 CUR_DIR=`pwd`
 cd $HOME
+
+#
+# Screen RC
+#
 if [ -f .screenrc ]; then
   echo 'Move Old .screenrc to screenrc.old'
   mv .screenrc screenrc.old
@@ -9,6 +13,9 @@ fi
 echo 'Copy .screenrc to ROOT'
 cp $CUR_DIR/.screenrc $HOME/.screenrc
 
+#
+# VI setup
+#
 if [ -f .vimrc ]; then
   echo 'Move Old .vimrc to vimrc.old'
   mv .vimrc vimrc.old
@@ -17,11 +24,15 @@ fi
 echo 'Copy .vimrc to ROOT'
 cp $CUR_DIR/.vimrc $HOME/.vimrc
 
-echo 'Copy ./util/* to ROOT'
-cp $CUR_DIR/util/ $HOME -R
-
 echo 'clone vundle.. '
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 echo 'Install plugins declared at Bundle'
 vim +BundleInstall +qall
+
+#
+# Utility script.
+#
+echo 'Copy ./util/* to ROOT'
+cp $CUR_DIR/util/ $HOME -R
+
