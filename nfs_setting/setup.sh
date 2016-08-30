f7702bf44c246ce688ec57a50c0cfcef4a0c4ef9
Sungguk Lim <sungguk.lim@sap.com>
2016-08-30 14:30:28 +0900

    Removing unnecessary /nfs_setting

--- nfs_setting/setup.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install nfs-kernel-server
-
-mkdir /share
-
-vi /etc/exports
-
-/share/ *(rw,sync,nohide,no_root_squash,insecure,subtree_check)
-
-sudo /etc/init.d/nfs-kernel-server restart
